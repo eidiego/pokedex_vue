90a655540ae579fbd3ebcd6f85315f78febce851
Diego Rodrigues <diegokod@gmail.com>
2022-04-22 14:14:18 -0300

    Add: Pokemons img

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,136 @@
 <template>
   <div class="container">
     <div class="row mt-2">
-      <!-- início do lado esquerdo -->
-      <div class="col mb-2">
-        Palco
-      </div>
-      <!-- fim do lado esquerdo -->
+      <div class="col mb-2">palco</div>
 
-      <!-- início do lado direito -->
-      <div class="col mb-2">
-        Pokédex
+      <!-- início lado direito -->
+      <div class="col mb-2 pokedex">        
+        <div class="row">
+          <div class="col">
+            <h1>Pokédex</h1>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <select class="form-select">
+              <option>Id crescente</option>
+              <option>Id decrescrente</option>
+              <option>De A - Z</option>
+            </select>
+          </div>
+        
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Pesquisar pokémon">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="pokedex-catalogo">
+
+            <!-- início listagem dinâmica -->
+            <div class="cartao-pokemon bg-grama">
+              <h1>1 Bulbasaur</h1>
+              <span>grama</span>
+              <div class="cartao-pokemon-img">
+                <img src="@/assets/img/pokemons/001.png">
+              </div>
+            </div>
+            <!-- fim listagem dinâmica -->
+
+          </div>
+        </div>
       </div>
-      <!-- fim do lado direito -->
+      <!-- fim lado direito -->
+
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
-  name: 'HomeView',
- }
+  name: 'HomeView'
+}
 </script>
+
+<style>
+body {
+  background-color: #dee3eb;
+}
+</style>
+
+<style scoped>
+.pokedex {
+  padding: 20px;
+  background-color: #ffffff;
+  -webkit-box-shadow: 2px 2px 10px rgba(200, 200, 200, 0.77);
+  -moz-box-shadow: 2px 2px 10px rgba(200, 200, 200, 0.77);
+  box-shadow: 2px 2px 10px rgba(200, 200, 200, 0.77);
+  border-radius: 10px;
+}
+
+.pokedex-catalogo {
+  overflow: auto;
+  display: flex;
+  flex-wrap: wrap;
+  height: 400px;
+  width: 98%;
+  margin-top: 10px;
+}
+
+.cartao-pokemon {
+  position: relative;
+  margin: 5px;
+  width: 150px;
+  height: 115px;
+  cursor: pointer;
+  border-radius: 5px;
+  -webkit-box-shadow: 2px 2px 2px rgba(200, 200, 200, 0.77);
+  -moz-box-shadow: 2px 2px 2px rgba(200, 200, 200, 0.77);
+  box-shadow: 2px 2px 2px rgba(200, 200, 200, 0.77);
+}
+
+.cartao-pokemon h1{
+  color:#fff;
+  font-size: 14px;
+  margin: 5px 0px 0px 5px;
+  padding: 0px;
+}
+
+.cartao-pokemon span{
+  color:#fff;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.3);
+  font-size: 12px;
+  margin: 10px 0px 0px 5px;
+  padding: 5px 10px 5px 10px;
+  border-radius: 25px;
+}
+
+.cartao-pokemon img {
+    max-width:60%;
+    max-height:60%;
+    float: right;
+}
+
+.bg-grama {
+  background-color: #2d8f78;
+}
+
+.bg-fogo {
+  background-color: #e47373
+}
+
+.bg-agua {
+  background-color: #5a9ed2
+}
+
+.bg-inseto {
+  background-color: #26d3ab
+}
+
+.bg-normal {
+  background-color: #cecece
+}
+
+</style>
